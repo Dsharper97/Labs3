3968cabb17beca3452413e2f546e2b6e0f51f778
Derrick Sharper <pfldsharper@gmail.com>
2019-10-02 13:51:51 -0400

    Added remaining colors to application some of them the RGB method need to be run in order to receive the color others I could just input the string name of the color.

--- app/src/main/java/edu/temple/labs3/ColorActivity.java
+++ app/src/main/java/edu/temple/labs3/ColorActivity.java
@@ -1,6 +1,7 @@
 package edu.temple.labs3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,7 +13,7 @@ import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
-public class ColorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ColorActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,24 +21,64 @@ public class ColorActivity extends AppCompatActivity implements AdapterView.OnIt
         setContentView(R.layout.activity_color);
 
 
-        //String colors [] = {"Red","Green","Blue", "Yellow", "Purple", "Gray", "Orange", "Brown", "Pink", "Teal" };
+        String colors [] = {"Red","Green","Blue", "Yellow", "Purple", "Gray", "Orange", "Brown", "Pink", "Teal" };
 
         //ArrayAdapter spinnerArray = new ArrayAdapter<>(ColorActivity.this, android.R.layout.simple_spinner_item, colors);
 
-        Spinner Colors = (Spinner) findViewById(R.id.spinner2);
+        Spinner Colors = findViewById(R.id.spinner2);
 
 // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colorSelection, android.R.layout.simple_spinner_item);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colorSelection, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        Colors.setAdapter(adapter);
-        Colors.setOnItemSelectedListener(this);
+       // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+
+        ColorAdapter CA = new ColorAdapter(ColorActivity.this, colors);
+
+
+        // Apply the adapter to the spinner
+
+            Colors.setAdapter(CA);
+
+        //Colors.setOnItemSelectedListener(this);
+        Colors.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ConstraintLayout background = findViewById(R.id.colorChange);
+                String text = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+                if (text.equals("Red")) {
+                    background.setBackgroundColor(Color.RED);
+                } else if (text.equals("Green")) {
+                    background.setBackgroundColor(Color.GREEN);
+                } else if (text.equals("Blue")) {
+                    background.setBackgroundColor(Color.BLUE);
+                } else if (text.equals("Yellow")) {
+                    background.setBackgroundColor(Color.YELLOW);
+                } else if (text.equals("Purple")) {
+                    background.setBackgroundColor(Color.parseColor("Purple"));
+                }else if(text.equals("Gray")) {
+                    background.setBackgroundColor(Color.GRAY);
+                }else if(text.equals("Orange")) {
+                    background.setBackgroundColor(Color.rgb(255, 165, 0));
+                }else if(text.equals("Brown")) {
+                    background.setBackgroundColor(Color.rgb(139, 69, 19));
+                } else if(text.equals("Pink")) {
+                    background.setBackgroundColor(Color.rgb(255,105,180));
+                }else if(text.equals("Teal")){
+                    background.setBackgroundColor(Color.parseColor("Teal"));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 
-    @Override
+    /*@Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         View background = findViewById(R.id.colorChange);
         String text = adapterView.getItemAtPosition(i).toString();
@@ -62,12 +103,11 @@ public class ColorActivity extends AppCompatActivity implements AdapterView.OnIt
             background.setBackgroundColor(Color.rgb(255,105,180));
         }else if(text.equals("Teal")){
             background.setBackgroundColor(Color.parseColor("Teal"));
-
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
-    }
+    }*/
 }
--- app/src/main/java/edu/temple/labs3/ColorAdapter.java
+++ app/src/main/java/edu/temple/labs3/ColorAdapter.java
@@ -1,6 +1,7 @@
 package edu.temple.labs3;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -17,11 +18,13 @@ public class ColorAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
+
         return colors.length;
     }
 
     @Override
     public Object getItem(int i) {
+
         return colors[i];
     }
 
@@ -34,6 +37,28 @@ public class ColorAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         TextView textView = new TextView(context);
         String colorValue = colors[i];
-        return view;
+        textView.setText(colorValue);
+        if (colorValue.equals("Red")) {
+            textView.setBackgroundColor(Color.RED);
+        } else if (colorValue.equals("Green")) {
+            textView.setBackgroundColor(Color.GREEN);
+        } else if (colorValue.equals("Blue")) {
+            textView.setBackgroundColor(Color.BLUE);
+        } else if (colorValue.equals("Yellow")) {
+            textView.setBackgroundColor(Color.YELLOW);
+        } else if (colorValue.equals("Purple")) {
+            textView.setBackgroundColor(Color.parseColor("Purple"));
+        }else if(colorValue.equals("Gray")) {
+            textView.setBackgroundColor(Color.GRAY);
+        }else if(colorValue.equals("Orange")) {
+            textView.setBackgroundColor(Color.rgb(255, 165, 0));
+        }else if(colorValue.equals("Brown")) {
+            textView.setBackgroundColor(Color.rgb(139, 69, 19));
+        } else if(colorValue.equals("Pink")) {
+            textView.setBackgroundColor(Color.rgb(255,105,180));
+        }else if(colorValue.equals("Teal")){
+            textView.setBackgroundColor(Color.parseColor("Teal"));
+        }
+        return textView;
     }
 }
